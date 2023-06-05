865255c467d97768e0bdb5fa0e254421d0f99b75
Figix <1202hyunjin@naver.com>
2023-06-06 01:53:24 +0900

    0606

--- week12/GameEngene.hpp
+++ week12/GameEngene.hpp
@@ -9,6 +9,8 @@
 #include <cmath>
 #include "GameManager.hpp"
 #include "Object.hpp"
+#include "Scene.hpp"
+#include "texturemappin.hpp"
 //#include "EnemyManager.hpp"
 //#include "Player.hpp"
 //#include "Texturemapping.hpp"
@@ -21,11 +23,13 @@
 
 using namespace std;
 namespace RunGame {
+
 	class GameLoop {
 	private:
 		chrono::system_clock::time_point startRenderTimePoint;
 		chrono::duration<double> renderDuration;
 		int sleeptime;
+		SceneManager* sm;
 	public:
 		GameLoop() {}
 		~GameLoop() {}
@@ -41,6 +45,25 @@ namespace RunGame {
 			// esc를 통해 게임 종료 확인
 			if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 				glfwSetWindowShouldClose(window, GL_TRUE);
+
+			if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {k_Space = true;}
+			else {k_Space = false;}
+
+			if (key == GLFW_KEY_ENTER && action == GLFW_PRESS) { k_Enter = true; }
+			else { k_Enter = false; }
+			
+			if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS) { k_Rarrow = true; }
+			else { k_Rarrow = false; }
+			
+			if (key == GLFW_KEY_UP && action == GLFW_PRESS) { k_Uarrow = true; }
+			else { k_Uarrow = false; }
+			
+			if (key == GLFW_KEY_LEFT && action == GLFW_PRESS) { k_Larrow = true; }
+			else { k_Larrow = false; }
+
+			if (key == GLFW_KEY_DOWN && action == GLFW_PRESS) { k_Darrow = true; }
+			else { k_Darrow = false; }
+
 			//아래는 예시용으로 작성함
 			/*if (key == GLFW_KEY_S && action == GLFW_PRESS) { pressS = true; }
 			else { pressS = false; }*/
@@ -83,15 +106,27 @@ namespace RunGame {
 			//이 곳에서 텍스쳐 매핑할 것들 처리하기
 			/*init_texture_GameStart();
 			init_texture_GameOver();*/
+			initialize_texture();
 			/*-----------------------------------------------------------------------------------------------------------*/
 
 			//게임 상대 관리에서 시작을 정리하는 단계
-			//GM->GameState = GM->GameStart;
-
+			sm = new SceneManager();
 		}
 		void Update(GLFWwindow* window) {
 			//게임 스테이트 관리 및 연산 처리
 			startRenderTimePoint = chrono::system_clock::now();
+			if (sm->GameState == GameStart) {
+				sm->GameStart_Update();
+				cout << "0업데이트" << endl;
+			}
+			else if(sm->GameState == inGame) {
+				sm->inGame_Update();
+				cout << "1업데이트" << endl;
+			}
+			else if (sm->GameState == GameOver) {
+				sm->GameOver_Update();
+				cout << "2업데이트" << endl;
+			}
 		}
 		void Render(GLFWwindow* window) {
 			float ratio;
@@ -100,30 +135,31 @@ namespace RunGame {
 			ratio = width / (float)height;
 
 
-			glClearColor(0.15, 0.15, 0.15, 1);
+			glClearColor(0, 0, 0, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
-			//if (GM->GameState == GM->GameStart) {		//게임시작!
-			//	DrawGameStart();
-			//}
-			//else if (GM->GameState == GM->inGame) {		//인게임!
-			//	PL->Draw();
-			//	EM->allDraw();
-			//}
-			//else if (GM->GameState == GM->GameOver) {	//게임오버!
-			//	DrawGameOver();
-			//}
-
-			glfwSwapBuffers(window);
-			//렌더링이 오래걸릴 경우 다 못칠하니 미리 칠해두고 그것을 현재있는것과 바꿈
-
-			glfwPollEvents();
-			//이벤트에서 중복된 이벤트들을 정리해서 전달해주는 것
-			//함수가 실행되는 순간마다 전달받는다.
 
 			glFlush();
 			glDisable(GL_TEXTURE_2D);
 			glFinish();
+
+			if (sm->GameState == GameStart) {
+				sm->GameStart_Render();
+				cout << "0렌더" << endl;
+			}
+			else if (sm->GameState == inGame) {
+				sm->inGame_Render();
+				cout << "1렌더" << endl;
+			}
+			else if (sm->GameState == GameOver) {
+				sm->GameOver_Render();
+				cout << "2렌더" << endl;
+			}
+			glfwSwapBuffers(window);
+			//렌더링이 오래걸릴 경우 다 못칠하니 미리 칠해두고 그것을 현재있는것과 바꿈
+			glfwPollEvents();
+			//이벤트에서 중복된 이벤트들을 정리해서 전달해주는 것
+			//함수가 실행되는 순간마다 전달받는다.
 
 			renderDuration = chrono::system_clock::now() - startRenderTimePoint;
 			sleeptime = (int)((FPS_30-renderDuration.count()) * 1000);
--- week12/Scene.hpp
+++ week12/Scene.hpp
@@ -1,12 +1,111 @@
-#pragma once
+#pragma 
+#pragma comment(lib, "opengl32.lib")
 #include <vector>
+#include <chrono>
+#include <thread>
+#include <cmath>
 #include "Object.hpp"
-namespace RunGame {
-	class Scene {
-		public:
-			int width, height;
-			std::vector<Object> v;
+#include "texturemappin.hpp"
 
-		
+#define FPS_30 1/30.f 
+#define FPS_60 1/60.f
+#define FPS_144 1/144.f
+
+using namespace std;
+
+namespace RunGame {
+	static bool k_Space;
+	static bool k_Enter;
+	static bool k_Uarrow;
+	static bool k_Rarrow;
+	static bool k_Darrow;
+	static bool k_Larrow;
+
+	enum GameState {
+		GameStart,
+		inGame,
+		GameOver
 	};
+	class SceneManager {
+	private:
+		int width, height;
+		std::vector<Object> v;
+		bool initcheck;
+	public:
+		int GameState; // 0:게임 스타트, 1:인 게임, 2:게임 오버
+		SceneManager() {
+			this->width = 1366;
+			this->height = 768;
+			this->GameState = GameStart;
+			this->initcheck = true;
+		}
+
+		void GameStart_init() {
+			this->initcheck = false;
+		}
+		void GameStart_Update() {
+			if (this->initcheck == true) { GameStart_init(); }
+			//윗줄은 init 확인용
+			if (k_Space == true || k_Enter == true) {
+				this->GameState = inGame;
+				this->initcheck = true;
+			}
+		}
+		void GameStart_Render() {
+			glBindTexture(GL_TEXTURE_2D, textures[0]);
+			glBegin(GL_QUADS);
+				glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, 0);
+				glTexCoord2f(1.0, 0.0); glVertex3f(1.0, -1.0, 0);
+				glTexCoord2f(1.0, 1.0); glVertex3f(1.0, 1.0, 0);
+				glTexCoord2f(0.0, 1.0); glVertex3f(-1.0, 1.0, 0);
+			glEnd();
+		}
+		void inGame_init() {
+			this->initcheck = false;
+		}
+		void inGame_Update() {
+			if (initcheck == true) { inGame_init(); }
+			//윗줄은 init 확인용
+			if (this->initcheck == true) { GameStart_init(); }
+			//윗줄은 init 확인용
+			if (k_Space == true || k_Enter == true) {
+				this->GameState = GameOver;
+				this->initcheck = true;
+			}
+		}
+		void inGame_Render() {
+
+		}
+		void GameOver_init() {
+			this->initcheck = false;
+		}
+		void GameOver_Update() {
+			if (initcheck == true) { GameOver_init(); }
+			//윗줄은 init 확인용
+			if (this->initcheck == true) { GameStart_init(); }
+			//윗줄은 init 확인용
+			if (k_Space == true || k_Enter == true) {
+				this->GameState = GameStart;
+				this->initcheck = true;
+			}
+		}
+		void GameOver_Render() {
+			glColor4f(0.5, 0.5, 0.5, 1);
+			//glEnable(GL_TEXTURE_2D);
+			//glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+			//glBindTexture(GL_TEXTURE_2D, textGameStart);
+			glBegin(GL_QUADS);
+			//glTexCoord2f(0.0, 0.0); 
+			glVertex3f(-1.0, -1.0, 0);
+			//glTexCoord2f(1.0, 0.0); 
+			glVertex3f(1.0, -1.0, 0);
+			//glTexCoord2f(1.0, 1.0);
+			glVertex3f(1.0, 1.0, 0);
+			//glTexCoord2f(0.0, 1.0); 
+			glVertex3f(-1.0, 1.0, 0);
+			glEnd();
+		}
+	};
+
+
 }
